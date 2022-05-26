218d3d2a18d51b4044bae08d3c1f746ce718be81
Adeleke Anuoluwapo <102337992+cryptanu@users.noreply.github.com>
2022-05-26 09:51:18 +0100

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x35D41B9aE4E56056e37448804eEe2Ec5b6Ca19E3";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
